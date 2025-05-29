84e30837f434401baaa4f23d2cd187763f965e85
lunomeu <milho135@gmail.com>
2025-05-29 15:15:53 -0300

    controller adicionada, falta corrigir erros

--- src/main/java/com/via/agencia_viagens/repository/ClienteRepository.java
+++ src/main/java/com/via/agencia_viagens/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.via.agencia_viagens.repository;
 import com.via.agencia_viagens.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
